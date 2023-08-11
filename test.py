103aa3f38c82497e66122e2240f8a0eb919fbf44
Maksim Aleksyuk <maleksjuk@yandex.ru>
2023-08-11 22:08:26 +0300

    add change_array_indexes; rename functions

--- test.py
+++ test.py
@@ -1,49 +1,66 @@
-from lxml import etree, objectify
+from lxml import etree
 import json
 
+def get_answer_object() -> dict:
+    return {
+        'xPath': '',
+        'start': 0,
+        'end': 0,
+        'name': '',
+        'decorCode': 'anamnesis',
+        'code': ''
+    }
 
-def get_xpath(tree, search_text, answer) -> None:
+
+def change_array_indexes(path: str, value: int) -> str:
+    start, end = 0, 0
+    start = path.find('[')
+    end = path.find(']')
+
+    while start != -1:
+        try:
+            num = int(path[start+1:end]) + value
+            path = path[:start+1] + str(num) + path[end:]
+        except ValueError:
+            pass
+        start = path.find('[', end)
+        end = path.find(']', start)
+
+    return path
+
+
+def solve(input_file, search_text) -> None:
+    tree = etree.parse(input_file)
+
     root = tree.getroot()
     ns = root.nsmap
     lst = tree.xpath(f'//*[contains(text(), "{search_text}")]')
-    item = lst[0]
-    # print(item.getparent())
-    prefix = '/' + root.tag + '/'
-    path = tree.getelementpath(item.getparent())    
-    full_path = (prefix + path).replace('{' + ns[None] + '}', '')
-    res = tree.findall(path)
-    
-    for element in item.getparent().iter(tag='{' + ns[None] + '}text'):
-        if (item.getparent() == element.getparent()):
-            # print(element.text)
-            # print(len(element.text))
-            anamnesis = element.text.strip()
-            break
-        # if (element.tag == '{urn:hl7-org:v3}text'):
-        # print("%s - %s" % (element.tag, element.text))
-        # print(element.getparent())
-            # break
-    
-    # print(item.getparent().findall("./*", namespaces=ns))
-    
-    # print(path)
-    # print(res)
+    answer_list = []
 
-    
-    # return full_path
-    answer[0]['xPath'] = full_path
-    answer[0]['name'] = anamnesis
-    answer[0]['end'] = len(anamnesis) 
+    for item in lst:
+        # print(item.getparent())
+        prefix = '/' + root.tag + '/'
+        path = tree.getelementpath(item.getparent())    
+        full_path = (prefix + path).replace('{' + ns[None] + '}', '')
+        # full_path = change_array_indexes(full_path, -1)
+        
+        for element in item.getparent().iter(tag='{' + ns[None] + '}text'):
+            if (item.getparent() == element.getparent()):
+                anamnesis = element.text.strip()
+                break
+            # if (element.tag == '{urn:hl7-org:v3}text'):
+            # print("%s - %s" % (element.tag, element.text))
+            # print(element.getparent())
+                # break
+        
+        answer = get_answer_object()
+        answer['xPath'] = full_path
+        answer['name'] = anamnesis
+        answer['end'] = len(anamnesis)
 
+        answer_list.append(answer)
 
-answer = [{
-    'xPath': '',
-    'start': 0,
-    'end': 0,
-    'name': '',
-    'decorCode': 'anamnesis',
-    'code': ''
-}]
+    return answer_list
 
 
 if __name__ == '__main__':
@@ -51,9 +68,7 @@ if __name__ == '__main__':
     input_file = './oldsessions/test.xml'
     search_text = 'АНАМНЕЗ'
     
-    tree = etree.parse(input_file)
-    
-    get_xpath(tree, search_text, answer)
+    answer = solve(input_file, search_text)
     
     print(answer)
 
