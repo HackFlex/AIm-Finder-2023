5a93106d9b7cebf8926accb04f321efa10492e4a
Maksim Aleksyuk <maleksjuk@yandex.ru>
2023-11-12 00:02:22 +0300

    check bounds of symptoms; add post-processing of text

--- src/final/outputNNHandler.py
+++ src/final/outputNNHandler.py
@@ -1,5 +1,15 @@
 import shlex
 import subprocess
+from enum import Enum
+
+class BoundStatus(Enum):
+    UNIQUE = 0
+    REPEAT = 1
+    INTERSECT = 2
+
+ALLOWED_SPECIAL_CHAR = {'/', ','}
+
+IS_PRINT_DEBUG = False
 
 class outNNHandler:
     def __init__(self):
@@ -22,17 +32,45 @@ class outNNHandler:
 
     def generateJsonSection(self, srcSection, outSection):
         arrSymptoms = []
-        # print(srcSection['name'])
+        bounds = []
+        text_base = srcSection['name']
+        
+        if IS_PRINT_DEBUG:
+            tmp_index = 0
+            tmp_step = 50
+            while tmp_index < len(text_base):
+                print(f'{tmp_index:3} | {text_base[tmp_index:tmp_index+tmp_step]}')
+                tmp_index += tmp_step
 
         for symptom in outSection['symptoms']:
 
             start_nn, end_nn = [int(item) for item in symptom]
             text_nn = outSection['text'][start_nn:end_nn]
+
             result = self.__get_real_bounds_and_text(
-                srcSection['name'], text_nn, start_nn, end_nn)
+                text_base, text_nn, start_nn, end_nn)
             if result is None:
                 continue
             start_base, end_base, text_symptom = result
+
+            current_bound = (start_base, end_base)
+            bound_status = self.__get_bound_status(bounds, current_bound)
+            if bound_status[0] != BoundStatus.UNIQUE:
+                if bound_status[0] == BoundStatus.REPEAT:
+                    if IS_PRINT_DEBUG:
+                        print(f'REPEAT: "{text_symptom}"')
+                    pass
+                elif bound_status[0] == BoundStatus.INTERSECT:
+                    index = bound_status[1][0]
+                    new_bound = bound_status[1][1]
+                    bounds[index] = new_bound
+                    arrSymptoms[index]['start'] = new_bound[0]
+                    arrSymptoms[index]['end'] = new_bound[1]
+                    arrSymptoms[index]['name'] = text_base[new_bound[0]:new_bound[1]]
+                    if IS_PRINT_DEBUG:
+                        print(f'INTERSECT: start={new_bound[0]} end={new_bound[1]} text="{text_base[new_bound[0]:new_bound[1]]}"')
+                continue
+            bounds.append(current_bound)
 
             symptomJson = self._getSymptomJson(
                 srcSection['xPath'],
@@ -42,14 +80,40 @@ class outNNHandler:
             arrSymptoms.append(symptomJson)
         return arrSymptoms
     
+    def __get_bound_status(self, bounds: list[tuple[int, int]],
+                           current_bound: tuple[int, int]
+                           ) -> tuple[BoundStatus, tuple[int, tuple[int, int]] or None]:
+        i = 0
+        for bound in bounds:
+            if current_bound == bound:
+                return BoundStatus.REPEAT,
+            if current_bound[0] < bound[1] < current_bound[1]:
+                return BoundStatus.INTERSECT, (i, (bound[0], current_bound[1]))
+            if current_bound[0] < bound[0] < current_bound[1]:
+                return BoundStatus.INTERSECT, (i, (current_bound[0], bound[1]))
+            i += 1
+        return BoundStatus.UNIQUE,
+
+    def __is_allowed_char(self, c: str) -> bool:
+        return c in ALLOWED_SPECIAL_CHAR or c.isalnum()
+    
+    def __is_correct_text(self, text: str) -> bool:
+        tmp_text = text.replace(' ', '')
+        for c in ALLOWED_SPECIAL_CHAR:
+            tmp_text = tmp_text.replace(c, '')
+        # print(tmp_text)
+        return len(tmp_text) > 0 and tmp_text.isalnum()
+    
     def __get_real_bounds_and_text(self, text_base: str, text_nn: str,
                                    start_nn: int, end_nn: int):
-        # print(f'start={start_nn} end={end_nn} text="{text_nn}"', end=' | ')
+        if IS_PRINT_DEBUG:
+            print(f'start={start_nn} end={end_nn} text="{text_nn}"', end=' | ')
 
-        # Плохо работает. Надо понять, как отсечь варианты, в которых нет цифр и букв
-        # if not text_nn.replace(' ', '').isalnum():
-        #     print("not is alnum")
-        #     return
+        # Надо понять, как отсечь варианты, в которых нет цифр и букв
+        if not self.__is_correct_text(text_nn):
+            if IS_PRINT_DEBUG:
+                print('This is the wrong text')
+            return
         
         # Выберем точку старта в основном тексте для поиска симптома
         index = text_base.rfind(' ', 0, start_nn)
@@ -86,7 +150,7 @@ class outNNHandler:
             # else:
             #     fragment = text_base[start_base:]
             # print(f'index={start_base} text="...{fragment}..."')
-        while start_base > 0 and text_base[start_base - 1].isalpha():
+        while start_base > 0 and self.__is_allowed_char(text_base[start_base - 1]):
             start_base -= 1
 
         # Найдём конец симптома в основном тексте
@@ -97,10 +161,11 @@ class outNNHandler:
                 # print(f'Next token "{token}" not found')
                 return
         end_base = index + len(tokens[-1])
-        while end_base < len(text_base) and text_base[end_base].isalpha():
+        while end_base < len(text_base) and self.__is_allowed_char(text_base[end_base]):
             end_base += 1
 
         text_symptom = text_base[start_base:end_base]
-        # print(f'start={start_base} end={end_base} text="{text_symptom}"')
+        if IS_PRINT_DEBUG:
+            print(f'start={start_base} end={end_base} text="{text_symptom}"')
 
         return start_base, end_base, text_symptom
