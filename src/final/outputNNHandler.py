3bf38a7bd6e9ec89844a7c3eb6e36c706fc9db5e
Maksim Aleksyuk <maleksjuk@yandex.ru>
2023-11-11 21:35:26 +0300

    fix left bound in word

--- src/final/outputNNHandler.py
+++ src/final/outputNNHandler.py
@@ -2,7 +2,7 @@ import shlex
 import subprocess
 
 class outNNHandler:
-    def __init(self):
+    def __init__(self):
         pass
 
     def _getSymptomJson(self, xPath, start, end, name) -> dict:
@@ -22,12 +22,17 @@ class outNNHandler:
 
     def generateJsonSection(self, srcSection, outSection):
         arrSymptoms = []
+        # print(srcSection['name'])
+
         for symptom in outSection['symptoms']:
 
             start_nn, end_nn = [int(item) for item in symptom]
             text_nn = outSection['text'][start_nn:end_nn]
-            start_base, end_base, text_symptom = self.__get_real_bounds_and_text(
+            result = self.__get_real_bounds_and_text(
                 srcSection['name'], text_nn, start_nn, end_nn)
+            if result is None:
+                continue
+            start_base, end_base, text_symptom = result
 
             symptomJson = self._getSymptomJson(
                 srcSection['xPath'],
@@ -39,7 +44,7 @@ class outNNHandler:
     
     def __get_real_bounds_and_text(self, text_base: str, text_nn: str,
                                    start_nn: int, end_nn: int):
-        print(f'start={start_nn} end={end_nn} text="{text_nn}"', end=' | ')
+        # print(f'start={start_nn} end={end_nn} text="{text_nn}"', end=' | ')
         
         # Выберем точку старта в основном тексте для поиска симптома
         index = text_base.rfind(' ', 0, start_nn)
@@ -61,14 +66,14 @@ class outNNHandler:
             token_start_nn = tokens[0]
             token_end_nn = tokens[-1]
         if not token_start_nn or not token_end_nn:
-            print('Tokens doesn\'t exist')
+            # print('Tokens doesn\'t exist')
             return
 
 
         # Найдём подстроку с симптомом в основном тексте
         start_base = text_base.find(token_start_nn, index)
         if start_base < 0:
-            print(f'Token "{token_start_nn}" not found')
+            # print(f'Token "{token_start_nn}" not found')
             return
         # else:
             # if start_base + 10 < len(text_base):
@@ -76,6 +81,8 @@ class outNNHandler:
             # else:
             #     fragment = text_base[start_base:]
             # print(f'index={start_base} text="...{fragment}..."')
+        while start_base > 0 and text_base[start_base - 1].isalpha():
+            start_base -= 1
 
 
         # Найдём конец симптома в основном тексте
@@ -83,11 +90,11 @@ class outNNHandler:
         for token in tokens:
             index = text_base.find(token, index)
             if index == -1:
-                print(f'Next token "{token}" not found')
+                # print(f'Next token "{token}" not found')
                 return
         end_base = index + len(tokens[-1])
 
         text_symptom = text_base[start_base:end_base]
-        print(f'start={start_base} end={end_base} text="{text_symptom}"')
+        # print(f'start={start_base} end={end_base} text="{text_symptom}"')
 
         return start_base, end_base, text_symptom
