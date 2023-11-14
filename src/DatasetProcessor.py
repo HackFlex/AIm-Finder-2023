11eb6fffd5858d35480fbe979835de4d61e86a68
knaumenko <knaumenko@npomis.ru>
2023-11-14 16:26:30 +0000

    Add aug data in test

--- src/DatasetProcessor.py
+++ src/DatasetProcessor.py
@@ -30,7 +30,7 @@ class DatasetProcessor():
             train_data["tokens"].extend(tmp["tokens"])
             train_data["ner_tags"].extend(tmp["ner_tags"])
         
-        tmp_test = Dataset.from_dict(train_data).train_test_split(test_size=0.1, seed=RANDOM_SEED)['test']
+        
 
         with open(self.dataset_path + 'augmentation.json') as user_file:
             aug_data = json.load(user_file)[0]
@@ -38,6 +38,9 @@ class DatasetProcessor():
         aug_num = len(aug_data["tokens"])
         print(aug_num)
         
+        tmp_test = Dataset.from_dict(train_data).train_test_split(test_size=0.1, seed=RANDOM_SEED)['test']
+        aug_test = Dataset.from_dict(aug_data).train_test_split(test_size=0.01, seed=RANDOM_SEED)['test']
+
         # inds = random.Random(RANDOM_SEED).sample(range(0, aug_num), 1000)
         # train_data["tokens"].extend(
         #     [token for i, token in enumerate(aug_data["tokens"]) if i in inds]
@@ -58,6 +61,9 @@ class DatasetProcessor():
 
         test_data["tokens"].extend(tmp_test["tokens"])
         test_data["ner_tags"].extend(tmp_test["ner_tags"])
+
+        test_data["tokens"].extend(aug_test["tokens"])
+        test_data["ner_tags"].extend(aug_test["ner_tags"])
 
         test_dataset = Dataset.from_dict(test_data)
         print('Total test raws:', len(test_dataset))
